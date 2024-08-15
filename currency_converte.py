cae0d18f36f72380a9907df91c8a3f91af973727
JoseAngelAz <angelvanitas555@gmail.com>
2024-08-14 20:11:53 -0600

    corregido alcance de las funciones por error de identacion y minorias en el main

--- currency_converte.py
+++ currency_converte.py
@@ -7,12 +7,12 @@ class CurrencyConverter:
         self.url = f"https://v6.exchangenerate-api.com/v6/{api_key}/latests/"
 
 #Definimos funcion para conseguir la conversion
-def get_exchange_rate(self,from_currency,to_currency):
-    response = requests.get(self.url + from_currency)
-    data = response.json()
-    if response.status_code == 200:
-        rates = data['conversion_rates']
-        return rates.get(to_currency,None)
-    else:
-        print(f"Error: {data['error-type']}")
-        return None
+    def get_exchange_rate(self,from_currency,to_currency):
+        response = requests.get(self.url + from_currency)
+        data = response.json()
+        if response.status_code == 200:
+            rates = data['conversion_rates']
+            return rates.get(to_currency,None)
+        else:
+            print(f"Error: {data['error-type']}")
+            return None
