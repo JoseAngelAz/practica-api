b947e02a7f3bb86fdcf855eb88f5db742295a023
JoseAngelAz <angelvanitas555@gmail.com>
2024-08-17 00:50:38 -0600

    correcion en if anidado de la variable rate, antes sumaba ahora multiplica

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ def main():
         if choice == '1':
             from_currency = input("Divisa de origen(por ejemplo, USD):").upper()
             to_currency = input("Divisa de destino (por ejemplo, EUR): ").upper()
-            amoutn = float(input(f"Cantidad en {from_currency,to_currency}: "))
+            amoutn = float(input(f"Cantidad en {from_currency}: "))
             rate = converter.get_exchange_rate(from_currency, to_currency)
 
             if rate:
-                converted_amount = amount + rate
+                converted_amount = amount * rate
                 print(f"{amount} {from_currency} = {converted_amount:.2f} {to_currency}")
         elif choice == '2':
             print("Saliendo de la calculadora...")
