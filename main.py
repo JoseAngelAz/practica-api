28038029b943da289b4aa41c614eb5b60ae457a2
José Angel Azucena <51334401+JoseAngelAz@users.noreply.github.com>
2024-08-17 14:31:14 +0000

    copie el codigo del ejemplo porque no funcionaba el mio, el error era la apikey, tuve que generarla nuevamente.

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-from currency_converte import CurrencyConverter
+from currency_converter import CurrencyConverter
 from config import API_KEY
 
 def main():
     converter = CurrencyConverter(API_KEY)
 
     while True:
-        print("\n--- CALCULADORA DE DIVISAS ---")
-        print("1. Convertir Divisas")
+        print("\n--- Calculadora de Divisas ---")
+        print("1. Convertir divisas")
         print("2. Salir")
 
-        choice  = input("ELige la opcion:\n")
-        if choice == '1':
-            from_currency = input("Divisa de origen(por ejemplo, USD):").upper()
-            to_currency = input("Divisa de destino (por ejemplo, EUR): ").upper()
-            amoutn = float(input(f"Cantidad en {from_currency}: "))
-            rate = converter.get_exchange_rate(from_currency, to_currency)
+        choice = input("Elige una opción: ")
 
+        if choice == '1':
+            from_currency = input("Divisa de origen (por ejemplo, USD): ").upper()
+            to_currency = input("Divisa de destino (por ejemplo, EUR): ").upper()
+            amount = float(input(f"Cantidad en {from_currency}: "))
+            rate = converter.get_exchange_rate(from_currency, to_currency)
             if rate:
                 converted_amount = amount * rate
                 print(f"{amount} {from_currency} = {converted_amount:.2f} {to_currency}")
@@ -23,7 +23,7 @@ def main():
             print("Saliendo de la calculadora...")
             break
         else:
-            print("Opcion no valida. Intente de nuevo.")
+            print("Opción no válida. Intenta de nuevo.")
+
 if __name__ == "__main__":
     main()
-
